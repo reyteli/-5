54b96e65b5f9dd1dc8ad5670886938d65af13836
Никита Олегович Романенко <nikcom526@mail.ru>
2024-01-02 14:04:41 +0300

    Добавьте файлы проекта.

---
+++ Практика 5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Практика_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[7];
+            int s = 0;
+            int k=0;
+            for (int i = 0; i < 7; i++)
+            {
+                 array[i] = Convert.ToInt32(Console.ReadLine());
+                s += array[i];
+                k++;
+            }
+            Console.WriteLine(((double)s)/k);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ практика 5.5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace практика_5._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность");
+            int n = Convert.ToInt32(Console.ReadLine()); 
+            int[,] array = new int[n,n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = ((i == j) || (i + j == n - 1) || (i + j == n - 3) || (j - i == n - 3) || (i + j== n + 1)) ? 1 : 0;
+            Console.Write("{0}", array[i, j]);
+
+                }
+        Console.WriteLine();
+
+            }
+    Console.ReadKey();
+	
+        }
+    }
+}
